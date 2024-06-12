04f5820bbdb36f650e3124145b2950fe7b19490f
HaDung2002 <87508098+HaDung2002@users.noreply.github.com>
2024-06-12 16:50:55 +0700

    Update project_variables.py

--- project_variables.py
+++ project_variables.py
@@ -4,5 +4,5 @@ from models import get_llm
 PROJECT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 DATA_DIR = os.path.join(PROJECT_PATH, 'data')
 SYTHETIC_DATA_DIR = os.path.join(PROJECT_PATH, 'synthetic_data')
-HF_TOKEN = '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'
+HF_TOKEN = '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'
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2"
