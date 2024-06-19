d40e8939df2c892deb370b6a2cbd1b4205acd1e1
HaDung2002 <87508098+HaDung2002@users.noreply.github.com>
2024-06-19 11:35:38 +0700

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -4,25 +4,29 @@ from project_variables import SYTHETIC_DATA_DIR
 import os
 import json
 
-with open(os.path.join(SYTHETIC_DATA_DIR, 'train_dataset.json')) as json_data:
-    dataset = json.load(json_data)
-    train_dataset = EmbeddingQAFinetuneDataset(queries=dataset['queries'], 
-                                               corpus=dataset['corpus'],
-                                               relevant_docs=dataset['relevant_docs'])
-    json_data.close()
-with open(os.path.join(SYTHETIC_DATA_DIR, 'val_dataset.json')) as json_data:
-    dataset = json.load(json_data)
-    val_dataset = EmbeddingQAFinetuneDataset(queries=dataset['queries'], 
-                                             corpus=dataset['corpus'],
-                                             relevant_docs=dataset['relevant_docs'])
-    json_data.close()
+def finetuning(epochs, batch_size, model_id="BAAI/bge-m3", model_output_path="finetuned-bge-m3"):
+    with open(os.path.join(SYTHETIC_DATA_DIR, 'train_dataset.json')) as json_data:
+        dataset = json.load(json_data)
+        train_dataset = EmbeddingQAFinetuneDataset(queries=dataset['queries'], 
+                                                  corpus=dataset['corpus'],
+                                                  relevant_docs=dataset['relevant_docs'])
+        json_data.close()
+    with open(os.path.join(SYTHETIC_DATA_DIR, 'val_dataset.json')) as json_data:
+        dataset = json.load(json_data)
+        val_dataset = EmbeddingQAFinetuneDataset(queries=dataset['queries'], 
+                                                 corpus=dataset['corpus'],
+                                                 relevant_docs=dataset['relevant_docs'])
+        json_data.close()
 
-finetune_engine = SentenceTransformersFinetuneEngine(
-    train_dataset,
-    model_id="BAAI/bge-m3",
-    model_output_path="finetuned-bge-m3",
-    val_dataset=val_dataset,
-    epochs = 100
-)
+    finetune_engine = SentenceTransformersFinetuneEngine(
+        train_dataset,
+        model_id=model_id,
+        model_output_path=model_output_path,
+        val_dataset=val_dataset,
+        epochs = epochs,
+        batch_size = batch_size
+    )
 
-finetune_engine.finetune()
+    finetune_engine.finetune()
+
+finetuning(epochs=10, batch_size=8)
